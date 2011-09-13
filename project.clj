998cc8adabea9f46212f70e9db34ec02a871c80b
Lee Hinman <lee@writequit.org>
2011-09-13 16:42:33 -0600

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject chunktest "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [aleph "0.2.0-beta1"]])
---
+++ test/chunktest/test/core.clj
@@ -0,0 +1,6 @@
+(ns chunktest.test.core
+  (:use [chunktest.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
